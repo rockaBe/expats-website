fa2b117a83d06a4a5f256e582ad34a7ba1c4983c
Bernd Naehler <bnaehler@babbel.com>
2021-01-24 22:24:03 +0100

    feat: setup basic service worker and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,24 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Expats",
+        short_name: "Expats",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/fleur-de-lis.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      }
+    },
+    {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
